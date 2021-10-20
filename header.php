1113f3bf3f6e86c56c4d22f10741c7ff2577302e
Asistente Sistemas <asistemas@GMN.COM>
2021-10-20 16:11:26 -0500

    CAMBIO PESTAÑA

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>FAME</title>
+    <title>FAME-PERÚ</title>
     <link rel="icon" href="img/logo.png" type="image/x-icon" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
